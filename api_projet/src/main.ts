58c1d0fe5c6b0020da2c07141c981e5a13fe5afd
PoLP0ot <107543417+PoLP0ot@users.noreply.github.com>
2023-12-03 13:35:00 +0100

    Version avec lien en ligne,Normalement tout fonctionne

--- api_projet/src/main.ts
+++ api_projet/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(RestaurantModule);
   app.useGlobalPipes(new ValidationPipe());
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT ?? 3000;
 
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
